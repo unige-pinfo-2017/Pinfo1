12bb09b988f690d4ba1f4e6fb24f40bcdf1348ec
candidj0 <joao.candido@etu.unige.ch>
2017-04-25 15:40:01 +0200

    Service et test sur log
    Ici j’ai j’ai mis en place le service User et dans LoginScreen j’ai mis
    en place un test simple, IDEAS/login/log

--- IDEAS/src/main/java/ch/unige/pinfo/user/User.java
+++ IDEAS/src/main/java/ch/unige/pinfo/user/User.java
@@ -5,12 +5,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import com.google.gson.Gson;
 
 @Entity
 @Table( name = "user")
+@NamedQueries({
+    @NamedQuery(name="User.findAll", query="SELECT e FROM User e"),
+    @NamedQuery(name="User.findByUsername", query="SELECT e FROM User e where e.username=:username"),
+}) 
 public class User {
 	// Fields:
 	@Id
