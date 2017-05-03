3a3004610f52866488efacb34786a8d691fc94ac
candidj0 <joao.candido@etu.unige.ch>
2017-05-03 23:22:49 +0200

    fix bug
    petit erreur c’est glissé -> corrigée

--- IDEAS/src/main/java/ch/unige/pinfo/wso2/Service/WSO2ServiceImpl.java
+++ IDEAS/src/main/java/ch/unige/pinfo/wso2/Service/WSO2ServiceImpl.java
@@ -14,7 +14,7 @@ public class WSO2ServiceImpl implements WSO2Service {
 	public State[] getPowerSensorState(String deviceId, String from, String to, TypePowerSocketSensors sensorType) {
 		String URL = "https://<wso2 ip address>:8243/powersocketubiquiti/device/stats/";
 		
-		String mock = "[{\"values\":{\"meta_owner\":\"admin\",\"meta_deviceType\":\"powersocketubiquiti\",\"meta_time\":1493290847263,\"testSensor\":0.0,\"_version\":\"1.0.0\",\"meta_deviceId\":\"qxoblpnqwzfp\"},\"id\":\"604604ce-7e3f-312a-8ed6-7fb8f2b32113\"},{\"values\":{\"meta_owner\":\"admin\",\"meta_deviceType\":\"powersocketubiquiti\",\"meta_time\":1493290855191,\"powerSensor\":1.98,\"_version\":\"1.0.0\",\"meta_deviceId\":\"qxoblpnqwzfp\"},\"id\":\"75bc0fb3-a1e0-35f2-970f-992650ad85c7\"}]";
+		String mock = "[{\"values\":{\"meta_owner\":\"admin\",\"meta_deviceType\":\"powersocketubiquiti\",\"meta_time\":1493290847263,\"powerSensor\":0.0,\"_version\":\"1.0.0\",\"meta_deviceId\":\"qxoblpnqwzfp\"},\"id\":\"604604ce-7e3f-312a-8ed6-7fb8f2b32113\"},{\"values\":{\"meta_owner\":\"admin\",\"meta_deviceType\":\"powersocketubiquiti\",\"meta_time\":1493290855191,\"powerSensor\":1.98,\"_version\":\"1.0.0\",\"meta_deviceId\":\"qxoblpnqwzfp\"},\"id\":\"75bc0fb3-a1e0-35f2-970f-992650ad85c7\"}]";
 		
 		Gson gson = new Gson();
 		State[] states = null;
