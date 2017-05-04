60a9d81996ad5b30672e10ad6fa9b40a5f8368be
Antoine <antoine.benquet@etu.unige.ch>
2017-05-04 16:11:49 +0200

    added dynamic topbar

--- ideas-angular/src/app/overview/topbar/topbar.component.ts
+++ ideas-angular/src/app/overview/topbar/topbar.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 
 export class TopbarComponent {
+	datas = DATA;
 }
+export class Data {
+	name: string;
+	valeur: number;
+	unite: string;
+}
+
+const DATA: Data[] = [
+	{name: 'Conso', valeur: 573, unite: 'kW/h'},
+	{name: 'Temperature', valeur: 25, unite: '°C'},
+	{name: 'elec', valeur: 450, unite: 'W'},
+	{name: 'blib blab', valeur: 24874, unite: 'sec'},
+
+];
