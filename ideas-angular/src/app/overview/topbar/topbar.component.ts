15f17473a14cf06a45d44bcd97c4eb5a99d9d2cd
Antoine <antoine.benquet@etu.unige.ch>
2017-05-04 16:55:23 +0200

    croix de preferences
    croix pour supprimer metriques + boutton « + »

--- ideas-angular/src/app/overview/topbar/topbar.component.ts
+++ ideas-angular/src/app/overview/topbar/topbar.component.ts
@@ -8,8 +8,15 @@ import { Component } from '@angular/core';
 
 export class TopbarComponent {
 	datas = DATA;
+	datash = DATAH;
 }
 export class Data {
+	name: string;
+	valeur: number;
+	unite: string;
+}
+
+export class DataHidden {
 	name: string;
 	valeur: number;
 	unite: string;
@@ -19,6 +26,12 @@ const DATA: Data[] = [
 	{name: 'Conso', valeur: 573, unite: 'kW/h'},
 	{name: 'Temperature', valeur: 25, unite: '°C'},
 	{name: 'elec', valeur: 450, unite: 'W'},
-	{name: 'blib blab', valeur: 24874, unite: 'sec'},
+	{name: 'blib blab', valeur: 24874, unite: 'sec'}
+];
 
+const DATAH: DataHidden[] = [
+	{name: 'Conso', valeur: 573, unite: 'kW/h'},
+	{name: 'Temperature', valeur: 25, unite: '°C'},
+	{name: 'elec', valeur: 450, unite: 'W'},
+	{name: 'blib blab', valeur: 24874, unite: 'sec'}
 ];
