02c9bf14d650a7bd9f8dfa4151bd73f03f82bd7d
QM Nguyen <quang-minh.nguyen@etu.unige.ch>
2017-05-08 15:27:42 +0200

    Fixed new framework bug

--- ideas-angular/src/app/table/table.component.ts
+++ ideas-angular/src/app/table/table.component.ts
@@ -2,29 +2,53 @@ import { Component } from '@angular/core';
 import { TableService } from './table.service';
 
 @Component({
-  	selector: 'basic-auto-demo',
-	templateUrl: './table-alt.component.html',
+  	selector: 'my-table',
+	templateUrl: './table.component.html',
 	styleUrls: [],
 	providers: [
 		TableService,
 	]
 })
 
-export class TableAltComponent {
+export class TableComponent {
 	private rows:any[] = [];
 	private columns:any[] = [];
+
+	private columnsExample:any[] = [
+	    { "prop": "deviceId" },
+	    { "prop": "power" },
+	    { "prop": "status" },
+		{ "prop": "current" }
+	];
+
+	private rowsExample:any[] = [
+		{
+			"deviceId":	"1",
+			"power":	"12.0",
+			"status":	"1.0",
+			"current": "4.0",
+		},
+		{
+			"deviceId":	"2",
+			"power":	"9.0",
+			"status":	"2.0",
+			"current": "9.0",
+		},
+	];
 
 	public constructor(
 		private tableService: TableService
 	) {}
 
+	public getDemo(){
+		this.columns = this.columnsExample;
+		this.rows = this.rowsExample;
+	}
+
 	public getDeviceTable(deviceType: string): void {
-		/*let promises: Promise<any>[] = [];
-		promises.push(this.getDeviceColumns(deviceType));
-		promises.push(this.getDeviceData(deviceType));
-		Promise.all(promises).then();*/
 		this.getDeviceColumns(deviceType);
 		this.getDeviceData(deviceType);
+
 	}
 
 	public getDeviceColumns(deviceType: string): Promise<any> {
