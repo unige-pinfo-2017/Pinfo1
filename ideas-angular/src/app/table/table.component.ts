e66ac2a339f026da95d81c7fa4c993d878edf252
QM Nguyen <quang-minh.nguyen@etu.unige.ch>
2017-05-12 21:12:48 +0200

    Added automatic refresh for table

--- ideas-angular/src/app/table/table.component.ts
+++ ideas-angular/src/app/table/table.component.ts
@@ -1,4 +1,7 @@
-import { Component, ViewEncapsulation, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from "rxjs";
+import { TimerObservable } from "rxjs/observable/TimerObservable";
+
 import { TableService } from './table.service';
 
 //@import '/node_modules/@swimlane/ngx-datatable/release/datatable.css';
@@ -22,17 +25,19 @@ import { TableService } from './table.service';
 	encapsulation: ViewEncapsulation.None
 })
 
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, OnDestroy {
 
 	@ViewChild('myTable') table: any;
 
+	private refreshRate: number = 20*1000; // Fréquence de rafraichissement en milliseconde
+	private subscription: Subscription;
+
+	private currentTable: string;
 	private rows:any[] = [];
 	private columns:any[] = [];
 	expanded: any = {};
 	selected: any[] = [];
 	timeout: any;
-
-
 
 	private columnsExample:any[] = [
 	    { "prop": "deviceId" },
@@ -65,7 +70,13 @@ export class TableComponent implements OnInit {
 	) {}
 
 	public ngOnInit(): void {
-		this.getTable("PowerSocket",1);
+		this.currentTable="PowerSocket";
+		this.getTable(this.currentTable, 1);
+		this.startTimer(5*1000);
+	}
+
+	public ngOnDestroy(): void {
+		this.subscription.unsubscribe();
 	}
 
 	public getDemo(){
@@ -78,10 +89,20 @@ export class TableComponent implements OnInit {
 		.then(arr => {
 			this.columns = arr[0];
 			this.rows = arr[1];
+		});
+		this.currentTable = deviceType;
+	}
+
+	public startTimer(refreshRate: number): void {
+		// Rafraichit la liste des données toutes les N millisecondes
+		let timer = TimerObservable.create(0, refreshRate);
+		this.subscription = timer.subscribe(t => {
+			this.getTable(this.currentTable, 1);
+			console.log("table refreshing");
 		})
 	}
 
-	public getDeviceTable(deviceType: string): void {
+	/*public getDeviceTable(deviceType: string): void {
 		this.getDeviceColumns(deviceType);
 		this.getDeviceData(deviceType);
 	}
@@ -96,7 +117,8 @@ export class TableComponent implements OnInit {
 		return this.tableService.getDeviceData(deviceType).then(
 			data => this.rows = data
 		);
-	}
+	}*/
+
 	onSelect(event: any) {
     	console.log('Event: select', event, this.selected);
   	}
