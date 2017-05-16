bcd273dded4113fffb3cf7f259dde2592fa42ada
Luca Joss <Luca.Joss@etu.unige.ch>
2017-05-16 11:23:55 +0200

    Added comments and modified pom.xml

--- IDEAS/src/test/java/ch/unige/pinfo/wso2/rest/WSO2WrapperTest.java
+++ IDEAS/src/test/java/ch/unige/pinfo/wso2/rest/WSO2WrapperTest.java
@@ -19,12 +19,15 @@ import ch.unige.pinfo.wso2.service.WSO2WrapperImpl;
 
 public class WSO2WrapperTest {
 	
+	// Mock of WSO2Client
 	@Mock
 	WSO2ClientRest mockClient;
 	
+	// Inject WSO2Client mock in the Wrapper
 	@InjectMocks
 	WSO2WrapperImpl wrapper = new WSO2WrapperImpl();
 	
+	// Initialize the mocks
 	@Before
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
@@ -33,16 +36,19 @@ public class WSO2WrapperTest {
 	@Test
 	public void testClientTrue(){
 		
+		// Define the tested values
 		String test = "[{\"values\":{\"meta_owner\":\"admin\",\"meta_deviceType\":\"powersocketubiquiti\",\"meta_time\":1493290847263,\"powerSensor\":0.0,\"_version\":\"1.0.0\",\"meta_deviceId\":\"qxoblpnqwzfp\"},\"id\":\"604604ce-7e3f-312a-8ed6-7fb8f2b32113\"},{\"values\":{\"meta_owner\":\"admin\",\"meta_deviceType\":\"powersocketubiquiti\",\"meta_time\":1493290855191,\"powerSensor\":1.98,\"_version\":\"1.0.0\",\"meta_deviceId\":\"qxoblpnqwzfp\"},\"id\":\"75bc0fb3-a1e0-35f2-970f-992650ad85c7\"}]";
 		JsonArray testJ = (JsonArray) Json.createReader(new StringReader(test)).readArray();
-		
 		String deviceType = "PowerSocket";
 		String sensorType = "powerSensor";
 		String deviceId = "1";
 		String from = "01.05.2017";
 		String to = "07.05.2017";
 		
+		// mock the getStates() function of the WSO2Client
 		when(mockClient.getStates(deviceType, deviceId, sensorType, from, to)).thenReturn(testJ);
+		
+		// Calculate the result which must be tested
 		double res = wrapper.getValue(deviceType, deviceId, sensorType, from, to);
 		
 		assertTrue(res == 1.98);
