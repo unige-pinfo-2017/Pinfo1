07bee2769520e1ae8c9fcfa00a6a0134ea6c779a
AzizFerchiou <azizferchiou@gmail.com>
2017-05-16 12:57:20 +0200

    Added editing-menu directory in table

---
+++ ideas-angular/src/app/table/editing-menu/editing-menu.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+	selector: 'editing-menu',
+	templateUrl: './editing-menu.component.html',
+	styleUrls: ['./editing-menu.component.css'],
+})
+
+
+const PARAM: any[] = [
+	{name: 'Brightness', value: 0, unit: 'kW/h'},
+	{name: 'Temperature', value: 25, unit: '°C'},
+	{name: 'elec', value: 450, unit: 'W'},
+	{name: 'blib blab', value: 24874, unit: 'sec'}
+	];
+
+
+export class EditingMenuComponent {
+	
+	var a: any;
+	
+	
+	
+}
+
+
+
