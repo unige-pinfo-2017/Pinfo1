0081b6a9c204d875c91a5ad95649c69d214b36a8
AzizFerchiou <azizferchiou@gmail.com>
2017-05-17 19:58:12 +0200

    Added styled editing-menu
    localhost:3000/editing-menu

--- ideas-angular/src/app/table/editing-menu/editing-menu.component.ts
+++ ideas-angular/src/app/table/editing-menu/editing-menu.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 
-
 const PARAM: any[] = [
 	{name: 'Brightness', value: 0, unit: 'kW/h'},
 	{name: 'Temperature', value: 25, unit: '°C'},
@@ -13,9 +12,12 @@ const PARAM: any[] = [
 		templateUrl: './editing-menu.component.html',
 		styleUrls: ['./editing-menu.component.css'],
 	})
+	
 
 export class EditingMenuComponent {
 	param = PARAM;
 	a: { state: string; value: string; } = { state: "Status", value: "ON" };
 
 }
+
+
