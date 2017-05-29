6992ed88d87302e87a0c48ce5bbee2708b80c12f
glausem0 <melanie.glauser@etu.unige.ch>
2017-05-29 14:03:34 +0200

    Add javadoc in LiveDataService + OverviewJsonBuilder + OverviewService

--- IDEAS/src/main/java/ch/unige/pinfo/overview/service/LiveDataService.java
+++ IDEAS/src/main/java/ch/unige/pinfo/overview/service/LiveDataService.java
@@ -9,8 +9,54 @@ import ch.unige.pinfo.overview.dom.LiveData;
 @Local
 public interface LiveDataService {
 	
+	/**
+	 * <b>addLiveData</b>
+	 * <p>
+	 * {@code void addLiveData(LiveData liveData)}
+	 * <p>
+	 * 
+	 * Add Live data in database.
+	 * 
+	 * @param liveData - The data Live to add
+	 */
 	void addLiveData(LiveData liveData);
+	
+	/**
+	 * <b>deleteLiveData</b>
+	 * <p>
+	 * {@code void deleteLiveData(Long id)}
+	 * <p>
+	 * 
+	 * Delete Live data from the database.
+	 * 
+	 * @param id - The live data's id.
+	 */
 	void deleteLiveData(Long id);
+	
+	/**
+	 * <b>getLiveDataById</b>
+	 * <p>
+	 * {@code LiveData getLiveDataById(Long id)}
+	 * <p>
+	 * 
+	 * Get live data by his id.
+	 * 
+	 * @param id - The live data's id.
+	 * @return
+	 * The live data.
+	 */
 	LiveData getLiveDataById(Long id);
+	
+	/**
+	 * <b>getAllLiveData</b>
+	 * <p>
+	 * {@code List<LiveData> getAllLiveData()}
+	 * <p>
+	 * 
+	 * Get all the existing data live.
+	 * 
+	 * @return
+	 * A list of Live data.
+	 */
 	List<LiveData> getAllLiveData();
 }
--- IDEAS/src/main/java/ch/unige/pinfo/overview/service/OverviewJsonBuilder.java
+++ IDEAS/src/main/java/ch/unige/pinfo/overview/service/OverviewJsonBuilder.java
@@ -10,6 +10,22 @@ import javax.json.JsonObject;
 import ch.unige.pinfo.overview.dom.LiveData;
 
 public class OverviewJsonBuilder {
+	
+	/**
+	 * <b>buildLiveDataJson</b>
+	 * <p>
+	 * {@code public JsonObject buildLiveDataJson(String measure, String unit, String value)} 
+	 * <p>
+	 * 
+	 * Build a {@code JsonObject} of live data containing the name of the measure data, 
+	 * the unit of the measure and the value.
+	 * 
+	 * @param measure - The name of the measure
+	 * @param unit - The unit of the unit.
+	 * @param value - The value.
+	 * @return
+	 * A {@code JsonObject} of the live data.
+	 */
 	public JsonObject buildLiveDataJson(String measure, String unit, String value) {
 		// Construit un JsonObject au format {name: measure, unit: unit, value: value}
 		return Json.createObjectBuilder()
@@ -19,6 +35,18 @@ public class OverviewJsonBuilder {
 				.build();
 	}
 	
+	/**
+	 * <b>buildHiddenDatas</b>
+	 * <p>
+	 * {@code public JsonArray buildHiddenDatas(List<LiveData> hiddenDatas)}
+	 * <p>
+	 * 
+	 * Build a {@code JsonArray} of the Hidden data. 
+	 * 
+	 * @param hiddenDatas - A list of the hidden data.
+	 * @return
+	 * A {@code JsonArray} of the hidden data.
+	 */
 	public JsonArray buildHiddenDatas(List<LiveData> hiddenDatas) {
 		JsonArrayBuilder builder = Json.createArrayBuilder();
 		for (LiveData hiddenData: hiddenDatas) {
@@ -27,6 +55,20 @@ public class OverviewJsonBuilder {
 		return builder.build();
 	}
 	
+	/**
+	 * <b>buildHiddenData</b>
+	 * <p>
+	 * {@code public JsonObject buildHiddenData(LiveData hiddenData)}
+	 * <p>
+	 * 
+	 * Build a {@code JsonObject} of the hidden data containing the name of data. 
+	 * The data is deducted from the live data.  
+	 * 
+	 * @param hiddenData - The live data to hide.
+	 * @return
+	 * a {@code JsonObject} of the hidden data.
+	 */
+
 	public JsonObject buildHiddenData(LiveData hiddenData) {
 		return Json.createObjectBuilder()
 				.add("name", hiddenData.getSensor().getMeasureName())
