14b4c57cd8a329d47b612831bd8fb922b1cc4aa8
Antoine <antoine.benquet@etu.unige.ch>
2017-05-30 16:44:29 +0200

    added some protractor tests

--- ideas-angular/src/test/e2e/filter.spec.js
+++ ideas-angular/src/test/e2e/filter.spec.js
@@ -6,23 +6,31 @@ describe("filter device list", function() {
             inputUsername.sendKeys("ideas");
             var inputPassword = element(by.css('input.password'))
             inputPassword.sendKeys("ideaspw");
-            
+
             browser.driver.sleep(1000).then(function(){
                 element.all(by.buttonText("Login")).click();
                 //element.all(by.buttonText("View device list")).click();
                 browser.driver.sleep(6000).then(function() {
-                    browser.get('http://localhost:3000/table/device');
-                    //var inputFilter = element(by.css('input.filter'))
-                    //inputFilter.sendKeys("ja");
+                    browser.ignoreSynchronization = true;
+                    //browser.get('http://localhost:3000/table/device');
+                    element.all(by.buttonText("View device list")).click();
+                    //
                     //element.all(by.buttonText("Overview")).click();
-
                     browser.driver.sleep(2000).then(function() {
-                        element.all(by.buttonText("Logout")).click();
+                        var inputFilter = element(by.css('input.filter'))
+                        browser.sleep(2000);
+                        inputFilter.sendKeys("Antoine");
+                        inputFilter.sendKeys(protractor.Key.ENTER);
+                        //element.all(by.buttonText("Overview")).click();
+                        browser.driver.sleep(9000).then(function() {
+                            var containFilter = element.all(by.css(".ngx-datatable"));
 
+                            expect(containFilter.count()).toEqual(1);
+                            //expect("Antoine test").toContain("Antoine");
+
+                        });
                     });
                 });
-                //browser.get('http://localhost:3000/overview');
-                                
             });
         });
     });
