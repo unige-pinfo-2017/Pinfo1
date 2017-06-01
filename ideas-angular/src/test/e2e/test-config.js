6087d16f53edd06b76e974eab8fada92aab992ad
Antoine <antoine.benquet@etu.unige.ch>
2017-06-01 15:39:34 +0200

    added test protractor

--- ideas-angular/src/test/e2e/test-config.js
+++ ideas-angular/src/test/e2e/test-config.js
@@ -1,6 +1,6 @@
 exports.config={
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['userBasic.spec.js'],
+    specs: ['sysAdminTest.spec.js'],
     capabilities: {
         browserName: 'firefox'
     },
--- ideas-angular/src/test/e2e/userBasic.spec.js
+++ ideas-angular/src/test/e2e/userBasic.spec.js
@@ -13,11 +13,8 @@ it("shouldn't go to user list", function(){
                 browser.sleep(2000);
                 browser.get('http://localhost:3000/table/user');
                 // means that it's impossible for a basic user to go to localhost:3000/table/user            
-                
-					
                 });
             }).then(function(){
-            	console.log(browser.getCurrentUrl());
             expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/overview');
             });
         
